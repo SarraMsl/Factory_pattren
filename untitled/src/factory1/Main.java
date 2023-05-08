863276dd776ba30408e1ac6467456ff44504d806
SarraMsl <saramoussalmal@gmail.com>
2023-05-08 16:36:20 +0200

    scanner in main

--- untitled/src/factory1/Main.java
+++ untitled/src/factory1/Main.java
@@ -1,14 +1,23 @@
 package factory1;
 
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        factory1.Transport transport= factory1.TransportFactory.Create_Transport(factory1.TransportFactory.Transport_Ship);
-        factory1.Transport transport1= factory1.TransportFactory.Create_Transport(factory1.TransportFactory.Transport_Truck);
-        transport.name();   transport.deliver();
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
 
-        transport1.name(); transport1.deliver();
-
+            System.out.print("Enter transport type (1 for ship, 2 for truck): ");
+            try {
+                int transportType1 = scanner.nextInt();
+                Transport transport1 = TransportFactory.Create_Transport(transportType1);
+                transport1.name();
+                transport1.deliver();
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input: Please enter a valid integer.");
+            } catch (Exception e) {
+                System.out.println("An error occurred: " + e.getMessage());
+            }
+        }
     }
 }
